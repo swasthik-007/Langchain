554aac4923c9bf75e4b3c2816fab93066fdd05b5
Swasthik <mohanty.swastik7@gmail.com>
2025-08-28 23:17:31 +0530

    #7

---
+++ 1_chat_models/1_chat_models_starter.py
@@ -0,0 +1,14 @@
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"),
+    model="llama-3.1-8b-instant",
+    temperature=0.01,  
+)
+
+result = llm.invoke("Hello, how are you?")
+print(result.content)
